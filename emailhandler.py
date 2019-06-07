3a4b901407816e5160990bd04a6af4fbb062deab
adityapathak343 <adityapathak343@users.github.com>
2019-06-07 23:15:39 +0530

    Added comment
    Added comment

--- emailhandler.py
+++ emailhandler.py
@@ -10,6 +10,7 @@ def __init__():
 
 
 def sendmail(sender, mailing_list, files_to_attach):
+    '''sends the attachments to the recipients provided'''
     yag.send(to=mailing_list, subject="You've got Mail!", 
              contents='Hello, from PyClass.\n' + sender + ' from PyClass has sent you the attached. \n'
                                                                               + 'Happy Learning!\n This is an automatically generated mail. The sender does not support replies.'
